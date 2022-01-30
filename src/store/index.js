ba8f52dd163ac7efe7b76586bd43d0f8c1e4c353
Drendal911 <76110710+Drendal911@users.noreply.github.com>
2022-01-30 13:58:01 -0600

    Modal & loading animation added

--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,9 @@ const stockInitialState = {
     dayHigh: 0,
     open: 0,
     previousClose: 0,
+    showModal: false,
+    modalText: "",
+    loadingSuggestions: true
 }
 
 function stockReducer(stock = stockInitialState, action) {
@@ -23,7 +26,42 @@ function stockReducer(stock = stockInitialState, action) {
             dayLow: action.incomingStock.dayLow.toFixed(2),
             dayHigh: action.incomingStock.dayHigh.toFixed(2),
             open: action.incomingStock.open.toFixed(2),
-            previousClose: action.incomingStock.previousClose.toFixed(2)
+            previousClose: action.incomingStock.previousClose.toFixed(2),
+            showModal: stock.showModal,
+            modalText: stock.modalText,
+            loadingSuggestions: stock.loadingSuggestions
+        };
+    }
+    if(action.type === 'modal') {
+        return {
+            symbol: stock.symbol,
+            name: stock.name,
+            price: stock.price,
+            changesPercentage: stock.changesPercentage,
+            change: stock.change,
+            dayLow: stock.dayLow,
+            dayHigh: stock.dayHigh,
+            open: stock.open,
+            previousClose: stock.previousClose,
+            showModal: action.incModal.showModal,
+            modalText: action.incModal.modalText,
+            loadingSuggestions: stock.loadingSuggestions
+        };
+    }
+    if(action.type === 'loading') {
+        return {
+            symbol: stock.symbol,
+            name: stock.name,
+            price: stock.price,
+            changesPercentage: stock.changesPercentage,
+            change: stock.change,
+            dayLow: stock.dayLow,
+            dayHigh: stock.dayHigh,
+            open: stock.open,
+            previousClose: stock.previousClose,
+            showModal: stock.showModal,
+            modalText: stock.modalText,
+            loadingSuggestions: action.loadingSuggestions
         };
     }
     return stock;
