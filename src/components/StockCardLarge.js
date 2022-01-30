90f8a804d1b7d07a80edf8b44216b757bb4e4e25
Drendal911 <76110710+Drendal911@users.noreply.github.com>
2022-01-30 06:38:36 -0600

    Footer & stock card css changes

--- src/components/StockCardLarge.js
+++ src/components/StockCardLarge.js
@@ -3,8 +3,7 @@ import {useSelector} from "react-redux";
 import {useState, useEffect} from "react";
 
 export default function StockCardLarge() {
-    //useSelector automatically subscribes the component to redux store and allows component to use the
-    //desired portion of the store only
+    //useSelector automatically subscribes the component to redux store and allows component to use the desired portion of the store only
     const stock = useSelector(stock => stock);
     const [stockChange, setStockChange] = useState(false);
 
@@ -51,33 +50,6 @@ export default function StockCardLarge() {
                     </div>
                 </div>
             }
-
-            {/*{stock.name === "" ? null :*/}
-            {/*    <div className={stockCard.main_container}>*/}
-            {/*        <div className={stockCard.card_container}>*/}
-            {/*            <div className={stockCard.name}>{stock.name}</div>*/}
-            {/*            <div className={stockCard.symbol}>{stock.symbol}</div>*/}
-            {/*            <div className={stockCard.price}>{stock.price} <span*/}
-            {/*                className={stockCard.price_span}> USD</span>*/}
-            {/*                {stockChange ?*/}
-            {/*                    <div className={stockCard.stock_change_container}>*/}
-            {/*                        <span*/}
-            {/*                            className={stockCard.price_change_green}>{stock.change} ({stock.changesPercentage})</span>*/}
-            {/*                        Today*/}
-            {/*                    </div>*/}
-            {/*                    :*/}
-            {/*                    <div className={stockCard.stock_change_container}>*/}
-            {/*                        <span*/}
-            {/*                            className={stockCard.price_change_red}>{stock.change} ({stock.changesPercentage})</span>*/}
-            {/*                        Today*/}
-            {/*                    </div>*/}
-            {/*                }*/}
-            {/*            </div>*/}
-            {/*            <div>High {stock.dayHigh}</div>*/}
-            {/*            <div>Low {stock.dayLow}</div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*}*/}
         </>
     )
 }
